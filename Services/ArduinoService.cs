6c1708501dfda669fa13fe324c557c7a05f351f7
admindpe-dharma <admindpe@dharmapacific.com>
2025-03-18 10:16:45 +0700

    Fix: Port closed loop

--- Services/ArduinoService.cs
+++ Services/ArduinoService.cs
@@ -188,6 +188,7 @@ namespace ScannerWeb.Services
                 }
                 catch(Exception ex)
                 {
+                    Disconnect().RunSynchronously();
                     logger.LogError(ex.Message);
                 }
             }            
