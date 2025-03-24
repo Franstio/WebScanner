453f8a6888b5a563e1962c75814b91674fc7c560
admindpe-dharma <admindpe@dharmapacific.com>
2025-03-24 10:49:42 +0700

    Update

--- Services/ArduinoService.cs
+++ Services/ArduinoService.cs
@@ -264,12 +264,12 @@ namespace ScannerWeb.Services
             leonardoPort.DtrEnable = true;
             logger.LogInformation($"Opening port in 1200 baud rate for reset");
             leonardoPort.Open();
-            await Task.Delay(1000);
+            await Task.Delay(5000);
             logger.LogInformation($"Port in 1200 baud rate status: {(leonardoPort.IsOpen ? "ON" : "OFF")}");
             leonardoPort.DtrEnable = false;
             leonardoPort.Close();
             logger.LogInformation($"Port in 1200 baud rate status: {(leonardoPort.IsOpen ? "ON" : "OFF")}");
-            await Task.Delay(1000);
+            await Task.Delay(5000);
         }
         public async Task CloseConnection()
         {
